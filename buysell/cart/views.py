781f77b2ecbf6cd8994473371beafe3765f05bc9
AllSafeCybercurity <skull32273227@gmail.com>
2021-05-24 07:47:41 -0700

    alipay fail

---
+++ buysell/cart/views.py
@@ -0,0 +1,24 @@
+#from django.shortcuts import render
+
+# Create your views here.
+#def index(request):
+ #   return render(request, 'index.html', {})
+
+from django.shortcuts import render
+from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
+
+from .models import Cart, CartItem
+
+class CartDetail(DetailView):
+    model = Cart
+    template_name='Cart'
+
+
+class CartList(ListView):
+    model = Cart
+    context_object_name = 'cart'
+    template_name='cart/cart_list.html'
+
+class CreateCart(CreateView):
+    model = Cart
+    template_name = 'cart/cart_create.html'    
